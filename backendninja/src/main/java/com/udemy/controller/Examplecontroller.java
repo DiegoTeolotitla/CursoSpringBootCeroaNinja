35ec87dd08a3a3e707e5e00ea6c48bc90ea97ec5
Diego.Teolotitla <diego.teolotitla@outlook.com>
2022-07-10 22:05:04 -0500

    Insertando Datos Complejos en plantillas

--- backendninja/src/main/java/com/udemy/controller/Examplecontroller.java
+++ backendninja/src/main/java/com/udemy/controller/Examplecontroller.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.udemy.model.Person;
+
 @Controller
 @RequestMapping("/example")
 public class Examplecontroller {
@@ -14,7 +16,7 @@ public class Examplecontroller {
 	//First example
 	@GetMapping("/exampleString")
 	public String exampleString(Model model) {
-		model.addAttribute("name", "Diego");
+		model.addAttribute("person", new Person("Diego Objeto", 23));
 		return EXAMPLE_VIEW;
 	}
 	
@@ -22,7 +24,7 @@ public class Examplecontroller {
 	@RequestMapping(value = "/exampleMAV", method = RequestMethod.GET)
 	public ModelAndView exampleMAV() {
 		ModelAndView mav = new ModelAndView(EXAMPLE_VIEW);
-		mav.addObject("name", "Diego Teolotitla");
+		mav.addObject("person", new Person("Diego Teolotitla Objeto", 28));
 		return mav;
 	}
 }
