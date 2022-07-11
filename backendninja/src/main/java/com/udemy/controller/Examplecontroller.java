f31c0b6748ff9086b1d80ac4ed4831ae6a88722a
Diego.Teolotitla <diego.teolotitla@outlook.com>
2022-07-10 21:55:36 -0500

    Insertando Datos Simples en Plantillas

--- backendninja/src/main/java/com/udemy/controller/Examplecontroller.java
+++ backendninja/src/main/java/com/udemy/controller/Examplecontroller.java
@@ -1,6 +1,7 @@
 package com.udemy.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,13 +13,16 @@ public class Examplecontroller {
 	public static final String EXAMPLE_VIEW = "example";
 	//First example
 	@GetMapping("/exampleString")
-	public String exampleString() {
+	public String exampleString(Model model) {
+		model.addAttribute("name", "Diego");
 		return EXAMPLE_VIEW;
 	}
 	
 	//Second example
 	@RequestMapping(value = "/exampleMAV", method = RequestMethod.GET)
 	public ModelAndView exampleMAV() {
-		return new ModelAndView(EXAMPLE_VIEW);
+		ModelAndView mav = new ModelAndView(EXAMPLE_VIEW);
+		mav.addObject("name", "Diego Teolotitla");
+		return mav;
 	}
 }
