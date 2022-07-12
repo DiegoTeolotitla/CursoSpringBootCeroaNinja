ecf3af82d9707dab7056ff9b6f9dfa138f19ffe2
Diego.Teolotitla <diego.teolotitla@outlook.com>
2022-07-11 23:24:38 -0500

    Pagina 404 - Not Found

---
+++ backendninja/src/main/java/com/udemy/controller/ExampleControllerFour.java
@@ -0,0 +1,20 @@
+package com.udemy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/examplefour")
+public class ExampleControllerFour {
+	
+	public static final String ERROR_VIEW = "404";
+	
+	@GetMapping("/error")
+	public ModelAndView errorPage() {
+		ModelAndView mav = new ModelAndView(ERROR_VIEW);
+		return mav;
+	}
+
+}
