fb44897fe08d0b66c8f0ba237ba84d9cab29f8ea
Diego.Teolotitla <diego.teolotitla@outlook.com>
2022-07-14 17:15:03 -0500

    Se agrega conexion a base de datos, se agrega el list courses y el add courses

--- backendninja/src/main/java/com/udemy/component/ExampleComponent.java
+++ backendninja/src/main/java/com/udemy/component/ExampleComponent.java
@@ -2,12 +2,20 @@ package com.udemy.component;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+
+import com.udemy.repository.CourseJpaRepository;
 
 @Controller("exampleComponent")
 public class ExampleComponent {
 	
 	public static final Log LOG = LogFactory.getLog(ExampleComponent.class);
+	
+	@Autowired
+	@Qualifier("courseJpaRepository")
+	private CourseJpaRepository courseJpaRepository;
 	
 	public void sayHello() {
 		LOG.info("Hello from component example");
