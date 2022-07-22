a1a5a478291763022f77a6d54681c4549a80712f
Diego.Teolotitla <diego.teolotitla@outlook.com>
2022-07-22 01:25:14 -0500

    Añador contacto - Vista y controller

--- backendninja/src/main/java/com/udemy/controller/ContactController.java
+++ backendninja/src/main/java/com/udemy/controller/ContactController.java
@@ -1,17 +1,37 @@
 package com.udemy.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.udemy.constant.ViewConstant;
+import com.udemy.model.ContactModel;
 
 @Controller("contactController")
 @RequestMapping("/contacts")
 public class ContactController {
-	
+	private static final Log LOG = LogFactory.getLog(ContactController.class);
+
+	@GetMapping("/cancel")
+	public String cancel() {
+		return ViewConstant.CONTACTS;
+	}
+
 	@RequestMapping("/contactform")
-	public String redirectContractForm() {
+	public String redirectContractForm(Model model) {
+		model.addAttribute("contactModel", new ContactModel());
 		return ViewConstant.CONTACT_FORM;
 	}
 
+	@PostMapping("/addcontact")
+	public String addContact(@ModelAttribute(name = "contactModel") ContactModel contactModel, Model model) {
+		LOG.info("METHOD : addContact() " + "PARAMS : " + contactModel.toString());
+		model.addAttribute("result", 1);
+		return ViewConstant.CONTACTS;
+	}
 }
